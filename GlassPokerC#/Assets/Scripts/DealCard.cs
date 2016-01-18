7ba72be9375dcc65d31be3c536c5b072d186ac43
Marcus Choi <marcuslchoi@gmail.com>
2016-01-18 12:38:56 -0600

    initial setup

---
+++ GlassPokerC#/Assets/Scripts/DealCard.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DealCard : MonoBehaviour {
+
+	public GameObject card;
+	public float speed;
+	public int order;
+
+	public Transform target;
+
+	void Deal()
+	{
+		// The step size is equal to speed times frame time.
+		var step = speed * Time.deltaTime;
+
+		// Move our position a step closer to the target.
+		transform.position = Vector3.Lerp(transform.position, target.position, step);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, step);
+	}
+
+
+//	IEnumerator Example() {
+//		print(Time.time);
+//		yield return new WaitForSeconds(2);
+//		print(Time.time);
+//	}
+
+	// Use this for initialization
+	void Start () {
+		//StartCoroutine (Example ());
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//Deal();
+		Invoke ("Deal", 0.2f*order);
+
+	}
+}
---
+++ GlassPokerC#/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	public void LoadScene(string scene)
+	{
+		//SceneManager.LoadScene (scene);
+		Application.LoadLevel(scene);
+
+	}
+}
