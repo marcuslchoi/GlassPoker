fd188457b62b55e5abbb9d2516eefcc4ca71c265
Marcus Choi <marcuslchoi@gmail.com>
2016-01-28 17:24:00 -0600

    testing syncvar, command, clientcallback
    to get text changes to sync over network (not working yet)

---
+++ GlassPokerC#/Assets/Scripts/TestTextChanged.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TestTextChanged : NetworkBehaviour {
+
+	//server automatically transmits this value to all clients when it changes
+	//[SyncVar]
+	private Text syncText;
+
+
+	// Update is called once per frame
+	void Start () {
+
+		syncText = GameObject.Find ("TestText").GetComponent<Text> ();
+
+		TransmitTextChange ();
+	}
+
+	//runs on the server, not in any clients
+	//[Command]
+	void CmdProvideNewTextToServer(string newString)
+	{
+		syncText.text = newString;
+
+	}
+
+	//runs only on the clients
+	//[ClientCallback]
+	void TransmitTextChange()
+	{
+		CmdProvideNewTextToServer ("CHANGED");
+
+	}
+}
+
